5c0ece59f03dfc05025e652cd2b342e34a680241
meghana <ganeshmeghana2@gmail.com>
2023-02-09 10:27:04 +0530

    L2 done

--- DAAL_SEM4-main/L2/Q1.c
+++ DAAL_SEM4-main/L2/Q1.c
@@ -2,6 +2,7 @@
 int main()
 {
     int m,n,t;
+    int opc = 0;
     printf("enter m and n:");
     scanf("%d %d",&m,&n);
     if(m>n)
@@ -10,14 +11,16 @@ int main()
         t = m;
     while(t != 0)
     {
+        (opc++);
         if(m % t == 0)
         {
             if(n % t == 0)
             {
-            printf("GCD is %d",t);
+            printf("GCD is %d\n",t);
             break;;
             }
         }
         t = t-1;
     }
+    printf("opc:%d",opc);
 }
